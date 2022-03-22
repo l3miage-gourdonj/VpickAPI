732adc4d58ff1672a4710a1fccb52eefc1c394d6
l3miage-gourdonj <jeremie.gourdon@etu.univ-grenoble-alpes.fr>
2022-03-22 11:04:17 +0100

    les velo oue

--- Vpick/src/main/java/com/cfmgg/vpick/configs/VeloConfig.java
+++ Vpick/src/main/java/com/cfmgg/vpick/configs/VeloConfig.java
@@ -8,6 +8,7 @@ import com.cfmgg.vpick.models.Velo;
 import com.cfmgg.vpick.repositories.BornetteRepository;
 import com.cfmgg.vpick.repositories.ModeleRepository;
 import com.cfmgg.vpick.repositories.VeloRepository;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,7 +19,6 @@ import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 @Configuration
@@ -56,17 +56,18 @@ public class VeloConfig {
                 Date date = Date.from(random.atStartOfDay(ZoneId.systemDefault()).toInstant());
 
                 Velo velo = new Velo(date, etat, statusVelo, modele);
+
+                veloRepository.save(velo);
                 bornette.setVelo(velo);
-                velos.add(velo);
+                bornetteRepository.save(bornette);
             }
-            veloRepository.saveAll(velos);
         };
     }
 
     /**
      * Generate a random date
      */
-    public static LocalDate between(LocalDate startInclusive, LocalDate endExclusive) {
+    public static @NotNull LocalDate between(LocalDate startInclusive, LocalDate endExclusive) {
         long startEpochDay = startInclusive.toEpochDay();
         long endEpochDay = endExclusive.toEpochDay();
         long randomDay = ThreadLocalRandom
