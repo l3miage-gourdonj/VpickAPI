1e8f27f4b7a5c7d9c1889bb7dae403d0afd400d1
l3miage-gourdonj <jeremie.gourdon@etu.univ-grenoble-alpes.fr>
2022-03-26 21:47:47 +0100

    Commentaire des enumerations

--- Vpick/src/main/java/com/cfmgg/vpick/enums/Etat.java
+++ Vpick/src/main/java/com/cfmgg/vpick/enums/Etat.java
@@ -1,5 +1,11 @@
 package com.cfmgg.vpick.enums;
 
+/**
+ * Cette enumeration liste les deux etat dans lesquels peuvent se trouver un velo ou une bornette
+ * ok -> tout va bien
+ * hs -> cest en panne
+ */
+
 public enum Etat {
     OK("ok"),
     HS("hs");
--- Vpick/src/main/java/com/cfmgg/vpick/enums/Sexe.java
+++ Vpick/src/main/java/com/cfmgg/vpick/enums/Sexe.java
@@ -1,5 +1,12 @@
 package com.cfmgg.vpick.enums;
 
+/**
+ * Sexe d'une personne
+ * homme
+ * femme
+ * possibilité d'en rajouter pour plaire a tout le monde si besoin
+ * (necessiterai quelque ajustement)
+ */
 public enum Sexe {
     FEMME("femme"),
     HOMME("homme");
